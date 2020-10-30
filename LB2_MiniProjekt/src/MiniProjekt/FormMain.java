07968619e3fe9c525188e98a15226a11ddd355e5
Shotacon <vampire@vampirecave.home>
2020-10-30 14:29:51 +0100

    Form

--- LB2_MiniProjekt/src/MiniProjekt/FormMain.java
+++ LB2_MiniProjekt/src/MiniProjekt/FormMain.java
@@ -1,8 +1,10 @@
 package MiniProjekt;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 public class FormMain extends JFrame {
     private JPanel mainPanel;
@@ -11,7 +13,7 @@ public class FormMain extends JFrame {
     public FormMain(String title) {
         super(title);
 
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // The application exits when the form is closed
         this.setContentPane(mainPanel);
         this.pack();
 
@@ -19,8 +21,20 @@ public class FormMain extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // Open folder to upload a file
-                
+                ReadFile();
             }
         });
+    }
+
+    private void ReadFile() {
+        FileDialog fileDialog = new FileDialog(this, "Upload a labyrinth", FileDialog.LOAD);
+        fileDialog.setVisible(true);
+        String filename = fileDialog.getFile();
+
+        if (filename != null) {
+            File labyrinthFile = new File(filename);
+
+            // Create labyrinth from file
+        }
     }
 }
--- LB2_MiniProjekt/src/MiniProjekt/LabyrinthBacktracker.java
+++ LB2_MiniProjekt/src/MiniProjekt/LabyrinthBacktracker.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 
 public class LabyrinthBacktracker {
     public static void main(String[] args) {
+        // Create and open form
         JFrame frame = new FormMain("Labyrinth Backtracker");
         frame.setSize(1700, 1100);
         frame.setVisible(true);
